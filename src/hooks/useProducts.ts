7cf65bd503cd893f3d93377a9d3dd52f306c3d4c
Dhanushka <dhanushkamcr@gmail.com>
2025-10-26 09:57:45 -0500

    Create react query to get categories

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -3,9 +3,15 @@ import { productService } from '../services/productService';
 import type { Item } from '../types/types';
 
 export const useProducts = () => {
-
     return useQuery<Item[]>({
         queryKey: ['items'],
         queryFn: productService.getAllProducts
     })
+};
+
+export const useCategories = () => {
+    return useQuery<string[]>({
+        queryKey: ['categories'],
+        queryFn: productService.getAllCategories
+    });
 };
