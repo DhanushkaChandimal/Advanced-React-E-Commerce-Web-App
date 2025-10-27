02609a2235f0ab010f6b7f7843ad4450fcd5fe66
Dhanushka <dhanushkamcr@gmail.com>
2025-10-26 23:41:23 -0500

    Add Cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,12 @@
+
+const Cart = () => {
+
+    return (
+        <div>
+            <h1 className="text-center">Your Cart</h1>
+            <p className="text-center align-self-center">No items in cart</p>
+        </div>
+    );
+}
+
+export default Cart;
