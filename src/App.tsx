0dfffa74b04cc131f0bed3d48b0b15105e9f721f
Dhanushka <dhanushkamcr@gmail.com>
2025-10-26 23:47:59 -0500

    Create routes for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Container from 'react-bootstrap/Container'
 import AppNavbar from './components/NavBar'
 import ProductList from './components/ProductList'
-import Container from 'react-bootstrap/Container'
+import Cart from './components/Cart'
 
 function App() {
 
   return (
     <Container className="py-4">
       <AppNavbar/>
-      <ProductList/>
+      <Routes>
+        <Route path='/products' element={<ProductList/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
     </Container>
   )
 }
